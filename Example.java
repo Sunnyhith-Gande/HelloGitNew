70123e979ea6a35143486825d445c8e1c81a062f
Gande <2505430@cognizant.com>
2026-05-20 16:04:36 +0530

    Added comment for Lab 5

--- Example.java
+++ Example.java
@@ -5,5 +5,6 @@ class Example
         System.out.print("HI");
         System.out.println("Second print statement");
         // This is a new comment added
+        //new comment is added
     }
 }
